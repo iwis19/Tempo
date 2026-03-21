4cf604b017324a0e2ff2ff321554a31c452fb3ba
Ronnie Gu <ronniegu2019@gmail.com>
2026-03-20 23:33:54 -0400

    Initial Commit

---
+++ Timint/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timint
+//
+//  Created by Glen gu on 2026-03-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timint/TimintApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimintApp.swift
+//  Timint
+//
+//  Created by Glen gu on 2026-03-20.
+//
+
+import SwiftUI
+
+@main
+struct TimintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
