eb787d19119824fb98fafca03c19c22e3baa66e7
Ronnie Gu <ronniegu2019@gmail.com>
2026-03-25 23:21:49 -0400

    added structured files, home, checkup, history, profile

---
+++ Tempo/CheckupPage.swift
@@ -0,0 +1,18 @@
+//
+//  CheckupPage.swift
+//  Tempo
+//
+//  Created by Glen gu on 2026-03-25.
+//
+
+import SwiftUI
+
+struct CheckupPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckupPage()
+}
---
+++ Tempo/ProfilePage.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePage.swift
+//  Tempo
+//
+//  Created by Glen gu on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ProfilePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePage()
+}
