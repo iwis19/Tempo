b039dc2e6a5fe2b780ee9a2256d68f9432721a2c
Ronnie Gu <ronniegu2019@gmail.com>
2026-04-16 02:04:49 -0400

    modularization of code and addition of reusable structs for pages

---
+++ Tempo/Views/StyleTemplates.swift
@@ -0,0 +1,18 @@
+//
+//  StyleTemplates.swift
+//  Tempo
+//
+//  Created by Glen gu on 2026-04-16.
+//
+
+import SwiftUI
+
+struct StyleTemplates: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StyleTemplates()
+}
